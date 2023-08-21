1a2a2def6cb05f1edc8e6d8941b38e80a5e7ce70
Caitlin <10053862+con-cat@users.noreply.github.com>
2023-08-21 21:56:34 +1000

    Configure Django admin
    This commit:
    - Registers the AccountTier and Image models in Django admin
    - Updates the User admin to extend it with fields from the Account model

--- src/images/admin.py
+++ src/images/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from images import models
+
+
+class AccountInline(admin.StackedInline):
+    model = models.Account
+    can_delete = False
+    verbose_name_plural = "account"
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom User admin with Account model fields inline.
+    """
+
+    inlines = [AccountInline]
+
+
+admin.site.register(models.AccountTier)
+admin.site.register(models.Image)
+# Re-register UserAdmin with AccountInline
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
